1ba73340d631d885bfe0add2b6f58ffa4e7e0108
Upen Manickam <uforupen@gmail.com>
2025-07-12 20:51:15 -0700

    comments

--- chatgpt.py
+++ chatgpt.py
@@ -3,6 +3,7 @@ from apikey import apikey
 import streamlit as st
 from langchain_openai import OpenAI
 
+# defined in the apikey.py as variable - added to .gitignore for privacy
 os.environ["OPENAI_API_KEY"] = apikey
 st.title("Medium Article Generator")
 topic = st.text_input("Input your topic of interest")
